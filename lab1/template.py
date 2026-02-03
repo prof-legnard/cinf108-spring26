e5ab3125adc4aba9f2c866be763a3fb9045dde23
tom_legnard <tom.legnard@unizin.org>
2026-02-03 15:06:37 -0500

    add lab1

---
+++ lab1/template.py
@@ -0,0 +1,59 @@
+"""
+#1
+Print the string "Hi my name is {your name}, this is my first print statement" in three different ways. Refer to the lecture notes on different ways of printing strings.
+Add your code below.
+"""
+
+name = ""
+
+
+"""
+# 2
+Copy your code from #1, edit the strings so that they match the format "Hi my name is {name}, this is print statement # {n}", where n is an integer value.
+Make sure that the number you are printing is of type int Add an extra line and 4 spaces to each print statement so the output looks like this:
+
+"Hi my name is Tom, this is print statement # 1."
+
+    "Hi my name is Tom, this is print statement # 2."
+
+        "Hi my name is Tom, this is print statement # 3."
+Add your code below.
+"""
+name = ""
+
+print("")
+print("")
+print("")
+
+"""
+4. How many hours are in the month of September? Write some python code to show your work and calculate the value. 
+Print the solution with the following statement, where `{num_days}` is the integer value of the number of days in September:
+
+
+There are {num_days} days in September
+Provide your code below.
+"""
+
+"""
+5. What  is 101 divided by 4, rounded down to the nearest integer? What is the remainder (modulo)?
+6. Show your calculation in python and print and label both values in one statement.
+"""
+
+
+
+
+
+"""BONUS (2 points): Print your name in block letters. each letter 
+should be at least 4 lines high:
+HINT: What is ASCII art?
+
+Example Output:
+ ____             __    _                                   _ 
+|  _ \ _ __ ___  / _|  | |    ___  __ _ _ __   __ _ _ __ __| |
+| |_) | '__/ _ \| |_   | |   / _ \/ _` | '_ \ / _` | '__/ _` |
+|  __/| | | (_) |  _|  | |__|  __/ (_| | | | | (_| | | | (_| |
+|_|   |_|  \___/|_|(_) |_____\___|\__, |_| |_|\__,_|_|  \__,_|
+                                  |___/                       
+"""
+
+
